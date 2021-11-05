9672dcf9e032e77f7d954ae58b69c76c3dba2980
Jomara12 <91356655+Jomara12@users.noreply.github.com>
2021-11-05 03:27:32 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>Jomara Torres</title>
+
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="imagen.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="paciente.php">Paciente</a></li>
+					<li><a href="receta.php">Receta</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
